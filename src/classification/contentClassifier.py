7838181d6cde48339a5a8dc1d16178479ee0c47d
Vertigo <soumyankarm@gmail.com>
2021-04-21 14:00:56 +0530

    Made API call for categorising articles

---
+++ src/classification/contentClassifier.py
@@ -0,0 +1,36 @@
+import argparse
+import io
+import json
+import os
+
+from google.cloud import language_v1
+import numpy
+import six
+
+def textClassify(text, verbose=True):
+    """Classify the input text into categories. """
+
+    language_client = language_v1.LanguageServiceClient()
+
+    document = language_v1.Document(
+        content=text, type_=language_v1.Document.Type.PLAIN_TEXT
+    )
+    response = language_client.classify_text(request={'document': document})
+    categories = response.categories
+
+    result = {}
+
+    for category in categories:
+        # Turn the categories into a dictionary of the form:
+        # {category.name: category.confidence}, so that they can
+        # be treated as a sparse vector.
+        result[category.name] = category.confidence
+
+    if verbose:
+        print(text)
+        for category in categories:
+            print(u"=" * 20)
+            print(u"{:<16}: {}".format("category", category.name))
+            print(u"{:<16}: {}".format("confidence", category.confidence))
+
+    return categories
