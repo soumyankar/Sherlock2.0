dc7d8e7f503226c45a0497a8c9c4ab2598539bd4
Vertigo <soumyankarm@gmail.com>
2021-04-21 14:01:48 +0530

    Output content catgories to front end

--- src/homepage.py
+++ src/homepage.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, url_for, Blueprint, render_template
 from extractNews import extractNews
 from tools.urlHandling import URLValidator
+from classification.contentClassifier import textClassify
 homepage = Blueprint("homepage", __name__, static_folder="../static", template_folder="../templates")
 
 @homepage.route("/", methods=['GET', 'POST'])
@@ -11,7 +12,8 @@ def index():
 		if(urlValidity == False):
 			return render_template('debug.html', urlValidity = urlValidity)
 		articleTitle, articleContent = extractNews(newsUrl)
-		return render_template('debug.html',urlValidity = urlValidity, articleTitle = articleTitle, articleContent = articleContent)
+		articleCategories = textClassify(articleContent)
+		return render_template('debug.html',urlValidity = urlValidity, articleTitle = articleTitle, articleContent = articleContent, articleCategories= articleCategories)
 	return render_template('index.html')	
 	if __name__=="__main__":
 		truth.run(debug=True)
