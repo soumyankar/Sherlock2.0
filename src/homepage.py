a0275fe7ca618a81e37db8cc506aa3a0437abd33
Vertigo <soumyankarm@gmail.com>
2021-05-01 00:27:33 +0530

    Rendered front end info for the feature extraction

--- src/homepage.py
+++ src/homepage.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, redirect, url_for, Blueprint, render_template
-from extractNews import extractNews
+from tools.extractNews import ExtractNews
 from tools.urlHandling import URLValidator
-from classification.contentClassifier import textClassify, sentimentClassify
-from classification.extractKeywords import ExtractKeywords
+from classification.contentClassifier import categoryClassify, sentimentClassify
+from classification.extractKeywords import ExtractKeywords, ExtractEntities
 homepage = Blueprint("homepage", __name__, static_folder="../static", template_folder="../templates")
 
 @homepage.route("/", methods=['GET', 'POST'])
@@ -12,11 +12,12 @@ def index():
 		urlValidity = URLValidator(newsUrl)
 		if(urlValidity == False):
 			return render_template('debug.html', urlValidity = urlValidity)
-		articleTitle, articleContent = extractNews(newsUrl)
-		articleCategories = textClassify(articleContent)
+		articleTitle, articleContent = ExtractNews(newsUrl)
+		articleCategories = categoryClassify(articleContent)
 		sentimentScore = sentimentClassify(articleContent)
 		articleKeywords = ExtractKeywords(articleContent)
-		return render_template('debug.html',urlValidity = urlValidity, articleTitle = articleTitle, articleContent = articleContent, articleCategories= articleCategories, sentimentScore= sentimentScore, articleKeywords= articleKeywords)
+		articleEntities = ExtractEntities(articleContent)
+		return render_template('debug.html',urlValidity = urlValidity, articleTitle = articleTitle, articleContent = articleContent, articleCategories= articleCategories, sentimentScore= sentimentScore, articleKeywords= articleKeywords, articleEntities = articleEntities)
 	return render_template('index.html')	
 	if __name__=="__main__":
 		truth.run(debug=True)
