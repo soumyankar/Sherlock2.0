2365b966b1af8b2c04d2ea1f1c9ffa971a63de4d
Vertigo <soumyankarm@gmail.com>
2021-05-12 13:24:02 +0530

    Parse JSON response from news api

--- src/tools/fetchNews.py
+++ src/tools/fetchNews.py
@@ -3,6 +3,7 @@ from urllib.parse import urlencode, quote_plus
 import urllib.parse
 import xml.dom.minidom
 import requests
+import json
 
 def FetchNews(keywords):
   # Find api_file
@@ -14,17 +15,43 @@ def FetchNews(keywords):
   # api_key = str(api_key[0].firstChild.nodeValue)
   api_key = '7f56d70b628f45f9b2de4355f63e6a24'
   for i in keywords.keys():
-  	for j in keywords[i]:
-  		qParameter = qParameter + ' OR ' + keywords[i]['text']
+  		qParameter = urllib.parse.quote(keywords[i]['keyword'], safe='') + ' AND ' + qParameter
 
+# We limit web crawling to 20 results because feature extraction might cost us a lot of time.
+  qParameter = qParameter[ :-5]
   parameters = {'q': qParameter,
   'language': 'en',
   'sortBy':'popularity',
   'apiKey': api_key
   }
-  url = 'https://newsapi.org/v2/top-headlines?'
-  urlTokens = urlencode(parameters, quote_via=urllib.parse.quote)
-  url = url + urlTokens
-  # print (url)
-  response = requests.get(url)
-  print (response.json())
+  # We must use the /everything endpoint because the q parameter does nnot work with /top-headlines
+  url = 'https://newsapi.org/v2/everything?'
+  queryParameters = urlencode(parameters, quote_via=urllib.parse.quote)
+  response = requests.get(url, params=queryParameters)
+  parsed_json = response.json()
+  totalResults = parsed_json['totalResults']
+  totalArticles = parsed_json['articles']
+  newsSources = []
+  newsSourcesCount = []
+  newsTitles = []
+  newsURLs = []
+  newsContents = []
+  print (totalResults)
+  for article in totalArticles:
+    articleSourceName = article['source']['name']
+    print (articleSourceName)
+    articleTitle = article['title']
+    articleURL = article['url']
+    articleContent = article['content']
+    if articleSourceName in newsSources:
+      index = newsSources.index(articleSourceName)
+      newsSourcesCount[index] += 1
+    if articleSourceName not in newsSources:
+      newsSources.append(articleSourceName)
+      newsSourcesCount.append(1)
+
+    newsTitles.append(articleTitle)
+    newsURLs.append(articleURL)
+    newsContents.append(articleContent[0:200]) # Limiting to 200 characters because we dont have the paid version of News API.
+
+  return newsContents
